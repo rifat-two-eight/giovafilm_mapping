355393210b0443f3693c994b2e3d47b8d8cac53f
tahfeezmizan <mizanahmed5316@gmail.com>
2026-04-14 04:59:41 +0600

    feat: create initial business registration form step 1 component

--- components/Common/for-business/business-form-step1.tsx
+++ components/Common/for-business/business-form-step1.tsx
@@ -268,12 +268,12 @@ export function BusinessFormStep1({ form }: BusinessFormStep1Props) {
           />
         </div>
 
-        {/* <div className="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center">
+        <div className="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center">
           <div className="text-center">
             <MapPin className="w-8 h-8 text-gray-400 mx-auto mb-2" />
             <p className="text-sm text-gray-500">CLICK TO SET PIN LOCATION</p>
           </div>
-        </div> */}
+        </div>
       </div>
 
       {/* Hours Section */}
